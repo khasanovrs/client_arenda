58299f265bbab43a44627a059cb7308092beae33
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-06-17 16:05:47 +0300

    Редактирование пользователя

--- src/app/components/users/interFaceNewUser.ts
+++ src/app/components/users/interFaceNewUser.ts
@@ -1,10 +1,12 @@
 interface InterFaceNewUser {
-  name: string;
-  lastName: string;
+  id: number;
+  fio: string;
+  name_lastName: string;
   pass: string;
   user_type: { val: number, name: string };
   branch_id: { val: number, name: string };
   email: string;
   phone: string;
   user_right: any;
+  btn: string;
 }
