a12c57507adbe774ad2d7d5a75a625f29b033215
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-08-12 19:24:07 +0300

    Написать скрипт обновления

---
+++ script/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'Запуск обновления';
+rm -rf dist;
+ng build;
+scp -r dist/client-arenda rdwork_linaz_front@rdwork.beget.tech:/home/r/rdwork/frontend-stroika-arenda
+ssh rdwork_linaz_front@rdwork.beget.tech '
+rm -rf public_html/*;
+mv client-arenda/* public_html/;
+rm -rf client-arenda;'
+echo 'Конец обновления';
